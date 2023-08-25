9271d493aaa3c1b6c9959d2bdd998a740a66383b
brook-bond <haykazvoskanyan@gmail.com>
2023-08-25 16:09:43 +0400

    crud

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +21,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
 });
+
+Route::get('products', [ProductController::class, 'index']); 
+Route::get('products/{id}', [ProductController::class, 'show']); 
+Route::post('/products', [ProductController::class, 'store']); 
+Route::put('productsupdate/{id}', [ProductController::class, 'update']);
+Route::delete('productdelete/{id}', [ProductController::class, 'destroy']);
